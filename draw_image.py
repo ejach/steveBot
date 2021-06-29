84fbeb55f03e0b8e5d1efbc349b2c36346214f0c
ejach <42357644+ejach@users.noreply.github.com>
2021-06-29 10:44:59 -0400

    Added the twitter integration

--- draw_image.py
+++ draw_image.py
@@ -5,14 +5,23 @@ from PIL import Image, ImageFont
 from PIL import ImageDraw
 from random_word import RandomWords
 
+# Global variable for the RandomWords function
 rw = RandomWords()
-word = rw.get_random_word(hasDictionaryDef="true", includePartOfSpeech="adjective,verb", minCorpusCount=1,
-                          maxCorpusCount=10,
-                          minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10)
 
 
+# Function for getting a random word from the RandomWords API
+def get_word():
+    word = rw.get_random_word(hasDictionaryDef="true", includePartOfSpeech="adjective,verb", minCorpusCount=1,
+                              maxCorpusCount=10,
+                              minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10)
+    return word
+
+
+# Function that handles writing the word from the API to the image
+# Saved as steve2.jpg, returns the word for use in the tweet status
 def draw_image():
-    global word
+    # Calls the get_word function
+    word = get_word()
     while True:
         try:
             # Open an Image
@@ -23,10 +32,11 @@ def draw_image():
             w, h = img_1.textsize(word)
             print('Width: ' + str(w) + ' height: ' + str(h))
 
-            # Add Text to an image
+            # Add text to an image
             font_path = 'fonts/impact.ttf'
             font = ImageFont.truetype(font_path, 150)
-            # Display top text
+            # Checks for (almost) centering the image depending on the length of the text
+            # TODO: Needs improvement
             if 45 < w < 55:
                 img_1.text((295, 1020), word, fill='white', font=font,
                            stroke_width=5, stroke_fill='black')
@@ -41,6 +51,7 @@ def draw_image():
             # Save the edited image
             img.save("steve2.jpg")
             # If typeError is thrown, get another random word
+            # This is a workaround for the API itself, there is no fix as of late
         except Exception as e:
             print(e)
             print('Restarting!')
@@ -51,3 +62,4 @@ def draw_image():
             continue
         else:
             break
+    return word
