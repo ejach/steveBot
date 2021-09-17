e4c7bed0d7adb77251e5ee438d1834df02582f98
poopi67 <42357644+ejach@users.noreply.github.com>
2021-09-17 12:54:58 -0400

    Code Refactoring

--- src/draw_image.py
+++ src/draw_image.py
@@ -18,22 +18,22 @@ def get_word():
 # Gets width of text and adjusts accordingly so it is not cut off
 def get_width(width):
     if 45 <= width < 55:
-        print("Under 55: Text offset detected, adjusting accordingly.")
+        print('Under 55: Text offset detected, adjusting accordingly.')
         return 295
     elif width < 45:
-        print("Under 45: Text offset detected, adjusting accordingly.")
+        print('Under 45: Text offset detected, adjusting accordingly.')
         return 340
     elif 60 < width < 70:
-        print("Over 60: Text offset detected, adjusting accordingly.")
+        print('Over 60: Text offset detected, adjusting accordingly.')
         return 205
     elif 70 <= width < 80:
-        print("Over 70: Text offset detected, adjusting accordingly.")
+        print('Over 70: Text offset detected, adjusting accordingly.')
         return 175
     elif 80 <= width < 90:
-        print("Over 80: Text offset detected, adjusting accordingly.")
+        print('Over 80: Text offset detected, adjusting accordingly.')
         return 110
     elif width >= 90:
-        print("Over 90: Text offset detected, adjusting accordingly.")
+        print('Over 90: Text offset detected, adjusting accordingly.')
         return 95
     else:
         return 230
@@ -62,12 +62,11 @@ def draw_image():
             img_1.text((height, 1020), word, fill='white', font=font,
                        stroke_width=5, stroke_fill='black')
             # Save the edited image
-            img.save("./assets/steve2.jpg")
+            img.save('./assets/steve2.jpg')
         # If typeError is thrown, get another random word
         # This is a workaround for the library itself, there is no fix as of late
         except TypeError as e:
-            print(e)
-            print('Restarting!')
+            print(str(e) + '\n' + 'Restarting!')
             sleep(1)
             word = get_word()
             continue
